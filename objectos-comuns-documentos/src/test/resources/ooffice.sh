8c4f5a21bcb67b1487463193bcd3b1e942c49b97
Ricardo Murad <ricardo.murad@objectos.com.br>
2012-11-01 15:44:11 -0200

    adiciona open office.sh

--- objectos-comuns-documentos/src/test/resources/ooffice.sh
+++ objectos-comuns-documentos/src/test/resources/ooffice.sh
@@ -16,5 +16,37 @@
 #
 # author ricardo.murad@objectos.com.br (Ricardo Murad)
 #
-/usr/lib64/openoffice/program/soffice.bin -headless -nofirststartwizard -accept="socket,host=localhost,port=8100;urp;StarOffice.Service"
-
+export OOFFICE_EXEC=/usr/lib64/openoffice/program/soffice.bin
+/usr/lib64/openoffice/program/soffice.bin -headless -nofirststartwizard -accept="socket,host=localhost,port=8100;urp;StarOffice.Service" &
+#PID=$(pidof -o %PPID $OOFFICE_EXEC)
+#case "$1" in
+#  start)
+#    stat_busy "Starting Net LibreOffice Daemon"
+#    [ -z "$PID" ] && /usr/lib64/openoffice/program/soffice.bin -headless -nofirststartwizard -accept="socket,host=localhost,port=8100;urp;StarOffice.Service" &> /dev/null
+#    if [ $? -gt 0 ]; then
+#      echo "starty"
+#    else
+#      add_daemon ooffice-net
+#      stat_done
+#    fi
+#    ;;
+#  stop)
+#    stat_busy "Stopping Net LibreOffice Daemon"
+#    [ ! -z "$PID" ] && kill $PID &> /dev/null
+#    if [ $? -gt 0 ]; then
+#      stat_fail
+#    else
+#      rm_daemon ooffice-net
+#      stat_done
+#    fi
+#    ;;
+#  restart)
+#    $0 stop
+#    sleep 1
+#    $0 start
+#    ;;
+#  *)
+#    echo "usage: $0 {start|stop|restart}"
+#esac
+#exit 0
+#
